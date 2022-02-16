6493bd1d9374196e8557099bfccd430691f71192
tafkos <30288817+tafkos@users.noreply.github.com>
2022-02-16 18:41:37 +0300

    Add second option to magic: customizable url
    if URL is not specified then use default

--- krokimagic/krokimagic.py
+++ krokimagic/krokimagic.py
@@ -12,16 +12,21 @@ class KrokiMagic(Magics):
                               'graphviz', 'vega', 'vega-lite', 'ditaa', 'svgbob'])
 
 
-    @cell_magic
     def kroki(self, line, cell):
         "simple wrapper for kroki.io"
-        
-        if line not in self.diagrams_supported:
+
+        splt_line = line.split(sep=' ')
+
+        if splt_line[0] not in self.diagrams_supported:
             raise NotImplemented
-            
+
         url = 'https://kroki.io'
-        r = requests.post(f'{url}/{line}/svg', json={'diagram_source': cell})
+        if len(splt_line) > 1:
+            url = splt_line[1]
+
+        r = requests.post(f'{url}/{splt_line[0]}/svg', json={'diagram_source': cell})
         if r.status_code == 200:
             return display(SVG(r.text))
         else:
             raise ValueError
+            
